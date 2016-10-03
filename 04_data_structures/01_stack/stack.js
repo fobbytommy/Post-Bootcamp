317afbe5577a438add45e67bb16949b19f536be3
Tommy Oh <tommyhyungjinoh@gmail.com>
2016-10-02 18:08:55 -0700

    stack has been added.

---
+++ 04_data_structures/01_stack/stack.js
@@ -0,0 +1,23 @@
+var Stack = (function() {
+	function Stack() {
+		this.dataStore = [];
+		this.top = 0;
+	}
+	Stack.prototype.push = function(item) {
+		this.dataStore[this.top] = item;
+		this.top++;
+	};
+	Stack.prototype.pop = function() {
+		this.top--;
+		this.dataStore.pop();
+	}
+	Stack.prototype.display = function() {
+		for (var i = 0; i < this.dataStore.length; i++) {
+			console.log(this.dataStore[i]);
+		}
+	}
+
+	return Stack;
+}());
+
+module.exports = Stack;
